434f9cc6ac9bbc94e71ff36839957f7fca27357b
roderikdeblock <41898873+roderikdeblock@users.noreply.github.com>
2021-03-23 15:02:49 +0100

    Add files via upload

---
+++ scripts/choco.ps1
@@ -0,0 +1,22 @@
+﻿$ChocolateyServerInstall = "http://<your_server>/install.ps1"
+$CholateyServer = "http://<your_server>/chocolatey"
+$APP1 = "<Your_App>"
+$APP2 = "<Your_App>"
+$APP3 = "<Your_App>"
+$APP4 = "<Your_App>"
+
+
+Set-ExecutionPolicy Bypass -Scope Process -Force ; iex ((New-Object System.Net.WebClient).DownloadString('$ChocolateyServerInstall'))
+
+
+
+choco install $APP1 -s $CholateyServer -y
+
+
+choco install $APP2 -s $ChocolateyServer -y
+
+
+choco install $APP3 -s $ChocolateyServer -y
+
+
+choco install $app4 -s $ChocolateyServer -y
