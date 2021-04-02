dd272929afa235ce484eea5e679789038829f59f
roderikdeblock <41898873+roderikdeblock@users.noreply.github.com>
2021-04-02 14:51:13 +0200

    Add files via upload

---
+++ HCL-UEFI-PVSCSI/scripts/Domain-Join.ps1
@@ -0,0 +1,6 @@
+﻿$Domain = "roderikdeblock"
+$DomainJoinUser = "administrator"
+$Password = "Thijmen7" | ConvertTo-SecureString -asPlainText -Force
+$JoinCredential = "$domain\$DomainJoinUser" 
+$Credential = New-Object System.Management.Automation.PSCredential($JoinCredential,$password)
+Add-Computer -DomainName $domain -Credential $JoinCredential -OUPath "OU=VDI,OU=Horizon,DC=roderikdeblock,DC=com" -Force
