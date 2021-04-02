ee5c0559d10cc548f2c83b4d61078916c7424b27
roderikdeblock <41898873+roderikdeblock@users.noreply.github.com>
2021-04-02 14:51:58 +0200

    Delete Domain-Join.ps1

--- HCL-UEFI-PVSCSI/scripts/Domain-Join.ps1
+++
@@ -1,6 +0,0 @@
-﻿$Domain = "roderikdeblock"
-$DomainJoinUser = "administrator"
-$Password = "Thijmen7" | ConvertTo-SecureString -asPlainText -Force
-$JoinCredential = "$domain\$DomainJoinUser" 
-$Credential = New-Object System.Management.Automation.PSCredential($JoinCredential,$password)
-Add-Computer -DomainName $domain -Credential $JoinCredential -OUPath "OU=VDI,OU=Horizon,DC=roderikdeblock,DC=com" -Force
